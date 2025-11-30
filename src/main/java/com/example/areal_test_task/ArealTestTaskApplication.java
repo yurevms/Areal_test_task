6bc2af4c44bdd3a9a7e29478a6a80717eb70e1e0
Maxim Yurev <yurev.3411@gmail.com>
2025-11-30 16:08:55 +0300

    Инициализация проекта, добавлены ключевые зависимости в pom.xml, настроен файл application.properties

---
+++ src/main/java/com/example/areal_test_task/ArealTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.areal_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArealTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArealTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/areal_test_task/ArealTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.areal_test_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArealTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
